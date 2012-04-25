31cca5bba7479cd0df1c8c375c80e52522d8374d
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-04-25 16:47:27 -0500

    Preparing this to be a seperate package

--- Makefile
+++ Makefile
@@ -4,9 +4,9 @@ VERSION=0.3
 
 .PHONY: all clean doc install debian test
 
-all: sign verify keygen test report.html doc dist/build/libHSopenpgp-$(VERSION).a dist/openpgp-$(VERSION).tar.gz
+all: sign verify keygen test report.html doc dist/build/libHSopenpgp-Crypto-$(VERSION).a dist/openpgp-Crypto-$(VERSION).tar.gz
 
-install: dist/build/libHSopenpgp-$(VERSION).a
+install: dist/build/libHSopenpgp-Crypto-$(VERSION).a
 	cabal install
 
 debian: debian/control
@@ -14,46 +14,46 @@ debian: debian/control
 test: tests/suite
 	tests/suite
 
-sign: examples/sign.hs Data/*.hs Data/OpenPGP/*.hs
+sign: examples/sign.hs Data/OpenPGP/Crypto.hs
 	ghc --make $(GHCFLAGS) -o $@ $^
 
-verify: examples/verify.hs Data/*.hs Data/OpenPGP/*.hs
+verify: examples/verify.hs Data/OpenPGP/Crypto.hs
 	ghc --make $(GHCFLAGS) -o $@ $^
 
-keygen: examples/keygen.hs Data/*.hs Data/OpenPGP/*.hs
+keygen: examples/keygen.hs Data/OpenPGP/Crypto.hs
 	ghc --make $(GHCFLAGS) -o $@ $^
 
-tests/suite: tests/suite.hs Data/*.hs Data/OpenPGP/*.hs
+tests/suite: tests/suite.hs Data/OpenPGP/Crypto.hs
 	ghc --make $(GHCFLAGS) -o $@ $^
 
-report.html: examples/*.hs Data/*.hs Data/OpenPGP/*.hs tests/*.hs
+report.html: examples/*.hs Data/OpenPGP/Crypto.hs tests/suite.hs
 	-hlint $(HLINTFLAGS) --report Data examples
 
-doc: dist/doc/html/openpgp/index.html README
+doc: dist/doc/html/openpgp-Crypto/index.html README
 
-README: openpgp.cabal
+README: openpgp-Crypto.cabal
 	tail -n+$$(( `grep -n ^description: $^ | head -n1 | cut -d: -f1` + 1 )) $^ > .$@
 	head -n+$$(( `grep -n ^$$ .$@ | head -n1 | cut -d: -f1` - 1 )) .$@ > $@
 	-printf ',s/        //g\n,s/^.$$//g\nw\nq\n' | ed $@
 	$(RM) .$@
 
-dist/doc/html/openpgp/index.html: dist/setup-config Data/OpenPGP.hs Data/OpenPGP/Crypto.hs
+dist/doc/html/openpgp-Crypto/index.html: dist/setup-config Data/OpenPGP/Crypto.hs
 	cabal haddock --hyperlink-source
 
-dist/setup-config: openpgp.cabal
+dist/setup-config: openpgp-Crypto.cabal
 	cabal configure
 
 clean:
 	find -name '*.o' -o -name '*.hi' | xargs $(RM)
-	$(RM) sign verify keygen tests/suite
+	$(RM) sign verify keygen tests/suite report.html
 	$(RM) -r dist dist-ghc
 
-debian/control: openpgp.cabal
+debian/control: openpgp-Crypto.cabal
 	cabal-debian --update-debianization
 
-dist/build/libHSopenpgp-$(VERSION).a: openpgp.cabal dist/setup-config Data/OpenPGP.hs Data/OpenPGP/Crypto.hs
+dist/build/libHSopenpgp-Crypto-$(VERSION).a: openpgp-Crypto.cabal dist/setup-config Data/OpenPGP/Crypto.hs
 	cabal build --ghc-options="$(GHCFLAGS)"
 
-dist/openpgp-$(VERSION).tar.gz: openpgp.cabal dist/setup-config Data/OpenPGP.hs Data/OpenPGP/Crypto.hs README
+dist/openpgp-Crypto-$(VERSION).tar.gz: openpgp-Crypto.cabal dist/setup-config Data/OpenPGP/Crypto.hs README
 	cabal check
 	cabal sdist
--- tests/suite.hs
+++ tests/suite.hs
@@ -4,7 +4,6 @@ import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
 import Test.HUnit hiding (Test)
 
-import Data.Word
 import Data.Binary
 import qualified Data.OpenPGP as OpenPGP
 import qualified Data.OpenPGP.Crypto as OpenPGP
@@ -13,19 +12,6 @@ import qualified Data.ByteString.Lazy.UTF8 as LZ (fromString)
 
 instance Arbitrary OpenPGP.HashAlgorithm where
 	arbitrary = elements [OpenPGP.MD5, OpenPGP.SHA1, OpenPGP.SHA256, OpenPGP.SHA384, OpenPGP.SHA512]
-
-testSerialization :: FilePath -> Assertion
-testSerialization fp = do
-	bs <- LZ.readFile $ "tests/data/" ++ fp
-	nullShield "First" (decode bs) (\firstpass ->
-			nullShield "Second" (decode $ encode firstpass) (\secondpass ->
-				assertEqual ("for " ++ fp) firstpass secondpass
-			)
-		)
-	where
-	nullShield pass (OpenPGP.Message []) _ =
-		assertFailure $ pass ++ " pass of " ++ fp ++ " decoded to nothing."
-	nullShield _ m f = f m
 
 testFingerprint :: FilePath -> String -> Assertion
 testFingerprint fp kf = do
@@ -53,101 +39,9 @@ prop_sign_and_verify secring kid halgo filename msg =
 	in
 		OpenPGP.verify secring (OpenPGP.Message [m,sig]) 0
 
-prop_s2k_count :: Word8 -> Bool
-prop_s2k_count c =
-	c == OpenPGP.encode_s2k_count (OpenPGP.decode_s2k_count c)
-
 tests :: OpenPGP.Message -> [Test]
 tests secring =
 	[
-		testGroup "Serialization" [
-			testCase "000001-006.public_key" (testSerialization "000001-006.public_key"),
-			testCase "000002-013.user_id" (testSerialization "000002-013.user_id"),
-			testCase "000003-002.sig" (testSerialization "000003-002.sig"),
-			testCase "000004-012.ring_trust" (testSerialization "000004-012.ring_trust"),
-			testCase "000005-002.sig" (testSerialization "000005-002.sig"),
-			testCase "000006-012.ring_trust" (testSerialization "000006-012.ring_trust"),
-			testCase "000007-002.sig" (testSerialization "000007-002.sig"),
-			testCase "000008-012.ring_trust" (testSerialization "000008-012.ring_trust"),
-			testCase "000009-002.sig" (testSerialization "000009-002.sig"),
-			testCase "000010-012.ring_trust" (testSerialization "000010-012.ring_trust"),
-			testCase "000011-002.sig" (testSerialization "000011-002.sig"),
-			testCase "000012-012.ring_trust" (testSerialization "000012-012.ring_trust"),
-			testCase "000013-014.public_subkey" (testSerialization "000013-014.public_subkey"),
-			testCase "000014-002.sig" (testSerialization "000014-002.sig"),
-			testCase "000015-012.ring_trust" (testSerialization "000015-012.ring_trust"),
-			testCase "000016-006.public_key" (testSerialization "000016-006.public_key"),
-			testCase "000017-002.sig" (testSerialization "000017-002.sig"),
-			testCase "000018-012.ring_trust" (testSerialization "000018-012.ring_trust"),
-			testCase "000019-013.user_id" (testSerialization "000019-013.user_id"),
-			testCase "000020-002.sig" (testSerialization "000020-002.sig"),
-			testCase "000021-012.ring_trust" (testSerialization "000021-012.ring_trust"),
-			testCase "000022-002.sig" (testSerialization "000022-002.sig"),
-			testCase "000023-012.ring_trust" (testSerialization "000023-012.ring_trust"),
-			testCase "000024-014.public_subkey" (testSerialization "000024-014.public_subkey"),
-			testCase "000025-002.sig" (testSerialization "000025-002.sig"),
-			testCase "000026-012.ring_trust" (testSerialization "000026-012.ring_trust"),
-			testCase "000027-006.public_key" (testSerialization "000027-006.public_key"),
-			testCase "000028-002.sig" (testSerialization "000028-002.sig"),
-			testCase "000029-012.ring_trust" (testSerialization "000029-012.ring_trust"),
-			testCase "000030-013.user_id" (testSerialization "000030-013.user_id"),
-			testCase "000031-002.sig" (testSerialization "000031-002.sig"),
-			testCase "000032-012.ring_trust" (testSerialization "000032-012.ring_trust"),
-			testCase "000033-002.sig" (testSerialization "000033-002.sig"),
-			testCase "000034-012.ring_trust" (testSerialization "000034-012.ring_trust"),
-			testCase "000035-006.public_key" (testSerialization "000035-006.public_key"),
-			testCase "000036-013.user_id" (testSerialization "000036-013.user_id"),
-			testCase "000037-002.sig" (testSerialization "000037-002.sig"),
-			testCase "000038-012.ring_trust" (testSerialization "000038-012.ring_trust"),
-			testCase "000039-002.sig" (testSerialization "000039-002.sig"),
-			testCase "000040-012.ring_trust" (testSerialization "000040-012.ring_trust"),
-			testCase "000041-017.attribute" (testSerialization "000041-017.attribute"),
-			testCase "000042-002.sig" (testSerialization "000042-002.sig"),
-			testCase "000043-012.ring_trust" (testSerialization "000043-012.ring_trust"),
-			testCase "000044-014.public_subkey" (testSerialization "000044-014.public_subkey"),
-			testCase "000045-002.sig" (testSerialization "000045-002.sig"),
-			testCase "000046-012.ring_trust" (testSerialization "000046-012.ring_trust"),
-			testCase "000047-005.secret_key" (testSerialization "000047-005.secret_key"),
-			testCase "000048-013.user_id" (testSerialization "000048-013.user_id"),
-			testCase "000049-002.sig" (testSerialization "000049-002.sig"),
-			testCase "000050-012.ring_trust" (testSerialization "000050-012.ring_trust"),
-			testCase "000051-007.secret_subkey" (testSerialization "000051-007.secret_subkey"),
-			testCase "000052-002.sig" (testSerialization "000052-002.sig"),
-			testCase "000053-012.ring_trust" (testSerialization "000053-012.ring_trust"),
-			testCase "000054-005.secret_key" (testSerialization "000054-005.secret_key"),
-			testCase "000055-002.sig" (testSerialization "000055-002.sig"),
-			testCase "000056-012.ring_trust" (testSerialization "000056-012.ring_trust"),
-			testCase "000057-013.user_id" (testSerialization "000057-013.user_id"),
-			testCase "000058-002.sig" (testSerialization "000058-002.sig"),
-			testCase "000059-012.ring_trust" (testSerialization "000059-012.ring_trust"),
-			testCase "000060-007.secret_subkey" (testSerialization "000060-007.secret_subkey"),
-			testCase "000061-002.sig" (testSerialization "000061-002.sig"),
-			testCase "000062-012.ring_trust" (testSerialization "000062-012.ring_trust"),
-			testCase "000063-005.secret_key" (testSerialization "000063-005.secret_key"),
-			testCase "000064-002.sig" (testSerialization "000064-002.sig"),
-			testCase "000065-012.ring_trust" (testSerialization "000065-012.ring_trust"),
-			testCase "000066-013.user_id" (testSerialization "000066-013.user_id"),
-			testCase "000067-002.sig" (testSerialization "000067-002.sig"),
-			testCase "000068-012.ring_trust" (testSerialization "000068-012.ring_trust"),
-			testCase "000069-005.secret_key" (testSerialization "000069-005.secret_key"),
-			testCase "000070-013.user_id" (testSerialization "000070-013.user_id"),
-			testCase "000071-002.sig" (testSerialization "000071-002.sig"),
-			testCase "000072-012.ring_trust" (testSerialization "000072-012.ring_trust"),
-			testCase "000073-017.attribute" (testSerialization "000073-017.attribute"),
-			testCase "000074-002.sig" (testSerialization "000074-002.sig"),
-			testCase "000075-012.ring_trust" (testSerialization "000075-012.ring_trust"),
-			testCase "000076-007.secret_subkey" (testSerialization "000076-007.secret_subkey"),
-			testCase "000077-002.sig" (testSerialization "000077-002.sig"),
-			testCase "000078-012.ring_trust" (testSerialization "000078-012.ring_trust"),
-			testCase "pubring.gpg" (testSerialization "pubring.gpg"),
-			testCase "secring.gpg" (testSerialization "secring.gpg"),
-			testCase "compressedsig.gpg" (testSerialization "compressedsig.gpg"),
-			testCase "compressedsig-zlib.gpg" (testSerialization "compressedsig-zlib.gpg"),
-			testCase "compressedsig-bzip2.gpg" (testSerialization "compressedsig-bzip2.gpg"),
-			testCase "onepass_sig" (testSerialization "onepass_sig"),
-			testCase "uncompressed-ops-dsa.gpg" (testSerialization "uncompressed-ops-dsa.gpg"),
-			testCase "uncompressed-ops-rsa.gpg" (testSerialization "uncompressed-ops-rsa.gpg")
-		],
 		testGroup "Fingerprint" [
 			testCase "000001-006.public_key" (testFingerprint "000001-006.public_key" "421F28FEAAD222F856C8FFD5D4D54EA16F87040E"),
 			testCase "000016-006.public_key" (testFingerprint "000016-006.public_key" "AF95E4D7BAC521EE9740BED75E9F1523413262DC"),
@@ -164,9 +58,6 @@ tests secring =
 		],
 		testGroup "Signing" [
 			testProperty "Crypto signatures verify" (prop_sign_and_verify secring "FEF8AFA0F661C3EE")
-		],
-		testGroup "S2K count" [
-			testProperty "S2K count encode reverses decode" prop_s2k_count
 		]
 	]
 
